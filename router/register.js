478e9ddb2e48a27bd96f25a8fa3b9fa9bee224b6
xuchang chen <imxuchang@gmail.com>
2017-01-22 00:14:35 -0800

    added email notification when register

--- router/register.js
+++ router/register.js
@@ -1,13 +1,24 @@
 // Module
 var koa = require('koa');
 var router = require('koa-router')();
+var nodemailer = require('@nodemailer/pro');
 var app = koa();
+
 
 // Config
 const config = require('../config');
 
 // Database
 var Users = require('../database/users');
+
+// create reusable transporter object using the default SMTP transport
+var transporter = nodemailer.createTransport({
+  service: config.email.service,
+  auth: {
+    user: config.email.email,
+    pass: config.email.password
+  }
+});
 
 // Route
 router.post('/create_user', function*() {
@@ -21,6 +32,21 @@ router.post('/create_user', function*() {
   
   try {
     yield Users.create(user);
+    // setup email data with unicode symbols
+    var mailOptions = {
+      from: '"Sporit" <no-reply@sporit.com>', // sender address
+      to: user.email, // list of receivers
+      subject: 'Welcome to sporit, ' + user.name + '!', // Subject line
+      html: '<b>Hello ' + user.name + ', you have registered in the sporit!</b>' // html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, function(err, info) {
+      if (err) {
+        return console.error(err);
+      }
+      console.log('Message %s sent: %s', info.messageId, info.response);
+    });
   } catch(e) {
     this.status(500);
   }
