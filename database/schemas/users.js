1c81890a4ad17577acda4e546129365fc3d751c2
xuchang chen <imxuchang@gmail.com>
2017-02-04 18:22:06 -0800

    fix rebase

--- database/schemas/users.js
+++ database/schemas/users.js
@@ -1,6 +1,6 @@
-var mongoose = require('mongoose');
-var db = require('../connection')();
-var bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
+const db = require('../connection')();
+const bcrypt = require('bcrypt');
 const SALT_WORK_FACTOR = 10;
 
 var UserSchema = new mongoose.Schema({
@@ -11,6 +11,7 @@ var UserSchema = new mongoose.Schema({
   name: String,
   password: String,
   status: String,
+  verify: String,
   course: [{
       course_id: {
       	type: mongoose.Schema.Types.ObjectId,
@@ -22,24 +23,17 @@ var UserSchema = new mongoose.Schema({
 UserSchema.pre('save', function(next) {
   var user = this;
 
-  // only hash the password if it has been modified (or is new)
   if (!user.isModified('password')) {
     return next();
   }
-
-  // generate a salt
   bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
     if (err) {
       return next(err);
     }
-
-    // hash the password using our new salt
     bcrypt.hash(user.password, salt, function(err, hash) {
       if (err) {
         return next(err);
       }
-
-      // override the cleartext password with the hashed one
       user.password = hash;
       next();
     });
