c72f630d6d1835c7a83f04b6f5701d0154217429
xuchang chen <imxuchang@gmail.com>
2017-02-05 00:53:08 -0800

    basic done

---
+++ service/email.js
@@ -0,0 +1,26 @@
+// Module
+const co = require('co');
+const nodemailer = require('@nodemailer/pro');
+const config = require('../config');
+
+// create reusable transporter object using the default SMTP transport
+var transporter = nodemailer.createTransport({
+  service: config.email.service,
+  auth: {
+    user: config.email.email,
+    pass: config.email.password
+  }
+});
+
+module.exports = function(mailOptions) {
+  return new Promise(function(resolve, reject) {
+    transporter.sendMail(mailOptions, function(err, info) {
+      if (err) {
+        console.error(err);
+        reject();
+      }
+      console.log('Message %s sent: %s', info.messageId, info.response);
+      resolve();
+    });
+  });
+};
