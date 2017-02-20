276a6bec090932109bb21e35b9bf02810fa73ef7
zhangkakashi <zhangkakashi@hotmail.com>
2017-02-19 16:44:36 -0800

    added video streamming api

--- router/video.js
+++ router/video.js
@@ -3,6 +3,9 @@ const koa = require('koa');
 const router = require('koa-router')();
 var app = koa();
 
+var fs = require("fs")
+var path = require("path");
+
 // Collection
 var Homework = require('../database/schemas/homeworks');
 var Videos = require('../database/schemas/videos');
@@ -54,6 +57,84 @@ router.post('/add_homework_to_video', function*() {
     } catch(e) {
       console.log(e);
     }
+});
+
+router.post('/display/:video_name', function*() {
+  console.log("[router.video] GET: display");
+  var video = yield Videos.find({"name": video_name});
+  var file = video.video_path;
+
+  fs.stat(file, function(err, stats) {
+
+    if(err)
+    {
+
+      console.log(err)
+      if(err.code === 'ENOENT')
+      {
+        this.status = 404;
+        return;
+      }
+      this.body = err;
+      return;
+    }
+
+    let range = req.headers.range;
+    //let range = req.body.range;
+    console.log(req.headers);
+    
+    if(!range)
+    {
+      let err = new Error("Wrong range");
+        this.status = 416;
+        this.body = {
+          error: true,
+          resposne: "Wrong range"
+        }
+      return ;
+    }
+
+    let positions = range.replace(/bytes=/, "").split("-");
+
+    let start = parseInt(positions[0], 10);
+
+    let file_size = stats.size;
+
+    console.log("positions[1] ===============>" + positions[1]);
+    console.log("file_size ===============>" + file_size);
+    let end = positions[1] ? parseInt(positions[1], 10) : file_size - 1;
+    console.log("end ===============>" + end);
+
+    let chunksize = (end - start) + 1;
+
+    this.set("Content-Range", "bytes " + start + "-" + end + "/" + file_size);
+    this.set("Accept-Ranges", "bytes");
+    this.set("Content-Length", chunksize);
+    this.set("Content-Type", "video/mp4");
+
+    let stream_position = {
+      start: start,
+      end: end
+    }
+
+
+    let stream = fs.createReadStream(file, stream_position)
+
+
+    stream.on("open", function() {
+
+      stream.pipe(this.body);
+
+    })
+
+    stream.on("error", function(err) {
+
+      return next(err);
+
+    });
+
+  });
+
 });
 
 // Export
