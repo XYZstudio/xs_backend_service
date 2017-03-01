310939acef3e73c1109b958db1f0936468230c1d
BinQi <bin@BinQis-MacBook-Pro.local>
2017-02-28 21:41:42 -0500

    register issue

--- router/register.js
+++ router/register.js
@@ -9,6 +9,8 @@ var Users = require('../database/schemas/users');
 
 // Route
 router.post('/create_user', function*() {
+  console.log('register!');
+  console.log(this.request.body);
   var req = this.request.body;
   var token = yield genToken(req.email);
   var user = {
@@ -20,21 +22,44 @@ router.post('/create_user', function*() {
   };
 
   try {
+    console.log('Checking exsiting user ...');
     var existingUser = yield Users.find({ email: req.email });
-    if (existingUser.length > 0) {
+
+    //Email already existed but not verified
+    if (existingUser.length > 0 && existingUser[0].status == 'inactive') {
+      console.log("Email already register but not verify");
+      this.body = {
+        warning: 'Email already register but not verified',
+        create: false
+      };
+      this.status = 500;
+      return;
+    }
+    
+    //User already existed
+    if (existingUser.length > 0 && existingUser[0].status == 'active') {
+      console.log('User already exsited!');
       this.body = {
         warning: 'The user already exists',
         create: false
       };
+      this.status = 500;
       return;
     } else {
+      console.log('Creating user...');
       yield Users.create(user);
     }
   } catch(e) {
+    console.log("Exception caught during register!");
+    this.body = {
+      warning: 'Unknown internal Error',
+      create: false
+    };
     this.status = 500;
   }
-  
+
   try {
+    console.log('Sending email...');
     var mailOptions = {
       from: '"Sporit" <no-reply@sporit.com>',
       to: user.email,
@@ -45,6 +70,7 @@ router.post('/create_user', function*() {
             'Click Here To Verify</a>'
     };
     yield sendEmail(mailOptions);
+    console.log('Email sent.');
   } catch(e) {
     // Ignore ...
   }
