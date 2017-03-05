c22c61b8ac486a41a87c5a811188ed8b1d1d0c1e
xuchang chen <imxuchang@gmail.com>
2017-03-05 12:16:31 -0800

    fix reset password

--- router/auth.js
+++ router/auth.js
@@ -3,6 +3,7 @@ const koa = require('koa');
 const router = require('koa-router')();
 const passport = require('koa-passport');
 const LocalStrategy = require('passport-local').Strategy;
+const genToken = require('../service/encrypt');
 const app = koa();
 const config = require('../config');
 var Users = require('../database/schemas/users');
@@ -57,6 +58,36 @@ router.post(
 router.get('/logout_user',function*(){
   this.logout();
   this.status = 200;
+});
+
+// Reset and update user's password
+// if the verification token and user email matches
+router.post('/reset_password', function*() {
+  const body = this.request.body;
+  var user = null;
+
+  try {
+    const newToken = yield genToken(body.password);
+    user = yield Users.findOne({ email: body.email, verify: body.token });
+    if (user) {
+      yield Users.update({ email: body.email, verify: body.token }, { password: newToken });
+    } else {
+      this.body = {
+        error: true,
+        message: '更新失败，请检查验证码是否正确'
+      };
+      return;
+    }
+  } catch(e) {
+    console.error(err);
+    this.status = 500;
+  }
+
+  this.body = {
+    error: false,
+    message: '成功更新了密码',
+    user: user,
+  };
 });
 
 // Export
