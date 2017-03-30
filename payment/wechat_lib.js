4672d73fff55b7015fdea4837c3f880f232eb969
xuchangchen <imxuchang@gmail.com>
2017-03-29 22:24:45 -0700

    set up basic order router and check router

---
+++ payment/wechat_lib.js
@@ -0,0 +1,74 @@
+const fs = require('fs');
+const path = require('path');
+const config = require('../config.json').payment.wechat;
+const WXPay = require('weixin-pay');
+const qr = require('qr-image');
+const wxpay = WXPay({
+	appid: config.appid,
+	mch_id: config.mch_id,
+	partner_key: config.partner_key
+});
+
+const order = function(total_fee, spbill_create_ip, product_id) {
+	return new Promise(function(resolve, reject) {
+		wxpay.createUnifiedOrder({
+			body: '课程购买',
+	  	out_trade_no: `${new Date().getTime()}${Math.random().toString().substr(2, 7)}`,
+	  	total_fee: total_fee,
+	  	spbill_create_ip: spbill_create_ip,
+	  	notify_url: 'http://www.sporit.cn/api/v1/received_wechat_pay',
+	  	trade_type: 'NATIVE',
+	  	product_id: product_id
+		}, function(err, res){
+			if (err) {
+				reject();
+			} else {
+				const qr_code = qr.imageSync(res.code_url, { type: 'png' }).toString('base64');
+				resolve(Object.assign(res, { qr_code: qr_code }));
+			}
+		});
+	});
+};
+
+// transaction_id: 32 length
+const check = function(transaction_id) {
+	return new Promise(function(resolve, reject) {
+		wxpay.queryOrder({ transaction_id: transaction_id }, function(err, order){
+			if (err) {
+				reject();
+			} else {
+				resolve(order);
+			}
+		});
+	});
+}
+
+const gen_qr = function(url) {
+	return qr.imageSync(url, { type: 'png' }).toString('base64');
+};
+
+// Promisify
+module.exports = {
+	order: order,
+	check: check,
+	gen_qr: gen_qr,
+};
+
+// wxpay.createUnifiedOrder({
+// 	body: '扫码支付测试',
+// 	out_trade_no: '20140703'+Math.random().toString().substr(2, 10),
+// 	total_fee: 1,
+// 	spbill_create_ip: '192.168.2.210',
+// 	notify_url: 'http://a63ff0e6.ngrok.io/api/v1/get_wechat_pay',
+// 	trade_type: 'NATIVE',
+// 	product_id: '1234567890'
+// }, function(err, res){
+// 	console.log(res);
+// 	const qr_code = qr.imageSync(res.code_url, { type: 'png' }).toString('base64');
+// 	console.log(qr_code);
+// });
+
+// look up an order
+// wxpay.queryOrder({ transaction_id:"4005522001201703295057330902" }, function(err, order){
+// 	console.log(order);
+// });
--- payment/wechat_router.js
+++ payment/wechat_router.js
@@ -1,12 +1,55 @@
 // Module
 const koa = require('koa');
 const router = require('koa-router')();
+const wechat_lib = require('./wechat_lib');
+const Courses = require('../database/schemas/courses');
 var app = koa();
 
 // Route
-// Wechat payment notification
-router.post('/get_wechat_pay', function*() {
-  console.log("[router.wechat_pay] POST: get_wechat_pay");
+// Wechat unified order
+router.post('/wechat/order', function*() {
+  console.log("[router.wechat_router] POST: /wechat/pay");
+  let body = this.request.body;
+  try {
+  	let total_fee = yield Courses.findOne({ name: body.product_id });
+	  total_fee = total_fee.fee;
+	  const product_id = body.product_id;
+	  const spbill_create_ip = body.spbill_create_ip;
+		const qrCode = yield wechat_lib.order(total_fee, spbill_create_ip, product_id);
+		this.body = qrCode;
+		return;
+  } catch(e) {
+  	console.error(e);
+  	this.body = {
+  		error: true,
+  		message: '购买失败'
+  	};
+  	return;
+  }
+});
+
+// Wechat check paid order
+router.post('/wechat/check', function*() {
+	console.log("[router.wechat_router] POST: /wechat/check");
+	let body = this.request.body;
+	try {
+		const order = yield wechat_lib.check(body.transaction_id);
+		this.body = order;
+		return;
+	} catch(e) {
+		console.error(e);
+		this.body = {
+			error: true,
+			message: '订单查询失败'
+		};
+		return;
+	}
+});
+
+// Wechat notify url response
+router.post('/received_wechat_pay', function*() {
+  console.log("[router.wechat_router] POST: /received_wechat_pay");
+  this.status = 200;
 });
 
 // Export
